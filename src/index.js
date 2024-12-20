3b71f3dab0e26bb96f507622af36e5b62a1382e4
Pratham <prathamshiv2005@gmail.com>
2024-12-20 13:28:41 +0530

    yoyo

--- src/index.js
+++ src/index.js
@@ -10,18 +10,17 @@ const websitePath = path.join(__dirname, "websites/views");
 const partialPath = path.join(__dirname, "websites/partials");
 
 
-
 app.set('view engine', "hbs");
 app.set('views', websitePath);
 hbs.registerPartials(partialPath)
 
 app.use(express.static(staticPath));
 
-app.get("/", (req,res)=>{
-    res.render("index",{
-        chanel: " man"
-    });
-})
+// app.get("/", (req,res)=>{
+//     res.render("index",{
+//         chanel: " man"
+//     });
+// })
 
 app.get("/", (req,res)=> {
     res.send("hello everyone");
